c5ddbb52d8556077c35848ecbdaa18c4daa0c106
tumingsheng928 <tumingsheng928@lu.com>
2018-03-29 16:18:42 +0800

    code refactor

--- popup.js
+++ popup.js
@@ -1,45 +1,140 @@
-/**
- * Created by distu on 2016/11/14.
- */
-document.addEventListener('DOMContentLoaded', function () {
-  var form = document.querySelector('#form');
-  var result = document.querySelector('#result');
-  var loading = document.querySelector('#loading');
-  form.addEventListener('submit', function (e) {
+document.addEventListener('DOMContentLoaded', function() {
+  const formElement = document.querySelector('#form');
+  const resultElement = document.querySelector('#result');
+  const loadingElement = document.querySelector('#loading');
+  const { action: url, method } = formElement;
+
+  formElement.addEventListener('submit', function(e) {
     e.preventDefault();
     e.stopImmediatePropagation();
-    toggle(loading);
-    var key = form['query'].value;
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState === 4) {
-        toggle(loading);
-        if(xhr.status === 200) {
-          result.innerText = JSON.stringify(extractResponse(xhr.responseText));
-        }
+    const { value: query } = formElement.elements['query'];
+    compose(
+      withLastWordNotModified,
+      withLoading,
+      generateContent,
+      withCache,
+      extractResponse
+    )(fetchTranslation)(query);
+  });
+
+  formElement.elements['query'].addEventListener('input', function(e) {
+    formElement.elements['submit'].disabled = !e.target.value;
+  });
+
+  /**
+   * If the word to be translated is the same as last word, nothing happens.
+   *
+   * @param {Function} next
+   * @returns
+   */
+  function withLastWordNotModified(next) {
+    return word => {
+      if (word !== formElement.dataset.lastWord) {
+        formElement.dataset.lastWord = word;
+        next(word);
       }
     };
-    xhr.open("GET", form.action + '?q=' + key, true);
-    xhr.send();
-  }, false);
+  }
 
-  form.querySelector('input').addEventListener('input', function (e) {
-    form.querySelector('button').disabled = !e.target.value;
-  });
-}, false);
+  /**
+   * Show loading while translating
+   *
+   * @param {Function} next
+   * @returns
+   */
+  function withLoading(next) {
+    return (...args) => {
+      toggle(loadingElement);
+      return next(...args).then(
+        () => {
+          toggle(loadingElement);
+        },
+        () => {
+          toggle(loadingElement);
+        }
+      );
+    };
+  }
 
-/**
- * extract json result from response html
- * @param responseText response html from bing dictionary
- * @returns {{word, pronounces, lang}}
- */
-function extractResponse(responseText) {
-  var temp = document.createElement('div');
-  temp.innerHTML = responseText;
-  var container = temp.querySelector('.qdef');
-  return [extractBasic, extractTranslate].reduce(function (result, current, index) {
-    return Object.assign(result, current(container.children[index]));
-  }, {});
+  /**
+   * Generate html using extracted translation info.
+   *
+   * @param {any} next
+   * @returns
+   */
+  function generateContent(next) {
+    return (...args) =>
+      next(...args).then(translation => {
+        resultElement.innerText = JSON.stringify(translation);
+      });
+  }
+
+  /**
+   * Cache translation history.
+   *
+   * @param {Function} next
+   * @returns
+   */
+  function withCache(next) {
+    const cache = new Map();
+    return key => {
+      if (cache.has(key)) return Promise.resolve(cache.get(key));
+      return Promise.resolve(next(key)).then(resp => {
+        cache.set(key, resp);
+        return resp;
+      });
+    };
+  }
+
+  /**
+   * Extract translation info from response html.
+   *
+   * @param {Function} next
+   * @returns
+   */
+  function extractResponse(next) {
+    return (...args) => {
+      return next(...args).then(response => {
+        const container = response.querySelector('.qdef');
+        return [extractBasic, extractTranslate].reduce(
+          (result, current, index) =>
+            Object.assign(result, current(container.children[index])),
+          {}
+        );
+      });
+    };
+  }
+
+  /**
+   * Get translation from bing site.
+   *
+   * @param {string} word
+   * @returns
+   */
+  function fetchTranslation(word) {
+    return new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.responseType = 'document';
+      xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4) {
+          if (xhr.status === 200) {
+            resolve(xhr.responseXML);
+          } else {
+            reject(xhr.status);
+          }
+        }
+      };
+      xhr.open(method, `${url}?q=${word}`, true);
+      xhr.send();
+    });
+  }
+});
+
+function compose(...fns) {
+  const fnList = fns.filter(fn => typeof fn === 'function');
+  if (fnList.length === 0)
+    throw new Error('Argument error, at least one `function` should be provided');
+  return fnList.reduce((f, g) => (...args) => f(g(...args)));
 }
 
 function extractBasic(doc) {
@@ -47,36 +142,46 @@ function extractBasic(doc) {
   var hd_p1_1 = doc.querySelector('.hd_p1_1');
   var lang = hd_p1_1.getAttribute('lang');
   var pronounceChildren = hd_p1_1.querySelectorAll('div');
-  var pronounces = pronounceChildren.length > 0 ? Array.prototype.reduce.call(pronounceChildren, function (result, current, index) {
-    if (index % 2 === 0) {
-      result.push([current])
-    } else {
-      result[result.length - 1].push(current);
-    }
-    return result;
-
-  }, []).map(function (divs) {
-    var pronounce = {
-      locale: divs[0].innerText
-    };
-    return pronounce;
-  }) : hd_p1_1.innerText;
+  var pronounces =
+    pronounceChildren.length > 0
+      ? Array.prototype.reduce
+          .call(
+            pronounceChildren,
+            function(result, current, index) {
+              if (index % 2 === 0) {
+                result.push([current]);
+              } else {
+                result[result.length - 1].push(current);
+              }
+              return result;
+            },
+            []
+          )
+          .map(function(divs) {
+            var pronounce = {
+              locale: divs[0].innerText,
+            };
+            return pronounce;
+          })
+      : hd_p1_1.innerText;
   return {
-    word: word,
-    pronounces: pronounces,
-    lang: lang
+    word,
+    pronounces,
+    lang,
   };
 }
 
 function extractTranslate(doc) {
-  return ({
-    translates: Array.prototype.map.call(doc.querySelectorAll('li'), function (item) {
-      return ({
+  return {
+    translates: Array.prototype.map.call(doc.querySelectorAll('li'), function(
+      item
+    ) {
+      return {
         pos: item.querySelector('.pos').innerText,
-        def: item.querySelector('.def').innerText
-      });
-    })
-  })
+        def: item.querySelector('.def').innerText,
+      };
+    }),
+  };
 }
 
 /**
@@ -84,6 +189,6 @@ function extractTranslate(doc) {
  * @param element html element
  */
 function toggle(element) {
-  var oldDisplay = element.style.display;
+  const oldDisplay = element.style.display;
   element.style.display = oldDisplay === 'none' ? 'block' : 'none';
 }
